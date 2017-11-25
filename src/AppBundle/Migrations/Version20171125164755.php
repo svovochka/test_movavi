bfff745081a8270ff7d84d0706ca1f60f340fd66
Vladimir M Semenov <svovochka@mail.ru>
2017-11-26 00:10:22 +0700

    Root category data migration

---
+++ src/AppBundle/Migrations/Version20171125164755.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171125164755 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('INSERT INTO `category`(`id`,`createdAt`,`updatedAt`,`title`,`slug`,`categoryId`,`root_id`,`lft`,`lvl`,`rgt`) VALUES (1,NOW(),NOW(),\'Root\',\'root\',NULL,1,1,0,2)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+    }
+}
