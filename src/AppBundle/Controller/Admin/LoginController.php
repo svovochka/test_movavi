d0ae9dbae68050215b07f5f6b6854e85c9bc8bf2
Vladimir M Semenov <svovochka@mail.ru>
2017-11-25 19:06:26 +0700

    Admin authentication

--- src/AppBundle/Controller/Admin/LoginController.php
+++ src/AppBundle/Controller/Admin/LoginController.php
@@ -5,15 +5,25 @@ namespace AppBundle\Controller\Admin;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use AppBundle\Controller\BaseController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends BaseController
 {
     /**
      * @Route("/admin/login", name="admin_login")
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, AuthenticationUtils $authUtils)
     {
-        // replace this example code with whatever you need
-        return $this->render('@Admin/login.html.twig');
+        // get the login error if there is one
+        $error = $authUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authUtils->getLastUsername();
+
+        return $this->render('@Admin/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ));
+
     }
 }
